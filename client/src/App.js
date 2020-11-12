b56b0c39207cfe02f2bca5de17f3d3a95191c503
JaimeSolisS <jaimesolissoto@me.com>
2020-11-12 16:06:01 -0700

    Update Redux state
    We get the currently loggedin user from firebase and based in auth state,
    redux state is populated with email and idToken to easily access the user information in the application.

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import React from "react";
+import React, {useEffect} from "react";
 import {Switch, Route} from "react-router-dom"
 import {toast, ToastContainer} from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
@@ -14,7 +14,35 @@ import Login from './pages/auth/Login'
 import  Header  from './components/nav/Header'
 import RegisterComplete from './pages/auth/RegisterComplete'
 
+import {auth} from './firebase'
+import {useDispatch} from 'react-redux'
+
+
 const App = () => {
+  const dispatch = useDispatch()
+
+  //check firebase auth state
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        const idTokenResult = await user.getIdTokenResult()
+       // console.log("user", user)
+        dispatch({
+          type: 'LOGGED_IN_USER', 
+          payload: {
+            email: user.email, //se puede agregar más info después
+            token: idTokenResult.token, 
+
+          },
+        });
+      }
+    });
+
+    //cleanup
+    return () => unsubscribe(); 
+
+  }, [])
 
   return (
     <>
--- client/src/reducers/userReducer.js
+++ client/src/reducers/userReducer.js
@@ -1,6 +1,3 @@
-//Get the currently loggedin user from firebase and based in auth state, 
-//populate redux state to easily access the user information in the application
-
 export function userReducer(state = null, action){
     switch(action.type){
         case "LOGGED_IN_USER":
