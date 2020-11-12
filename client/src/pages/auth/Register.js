7d8a1e201b07d5f5d93262b64bb7f263468eccfb
JaimeSolisS <jaimesolissoto@me.com>
2020-11-11 21:19:31 -0700

    Feat Email Link to Register
    npm i react-toastify

--- client/src/pages/auth/Register.js
+++ client/src/pages/auth/Register.js
@@ -2,16 +2,32 @@ import { Button } from 'antd';
 import React, {useState} from 'react'
 import styled from 'styled-components'
 import {auth} from '../../firebase'
-
+import {toast, ToastContainer} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
  const Register = () => {
 
   
      const [email, setEmail] = useState("");
 
-    const handleSubmit = () => {
-        //
-    };
+    const handleSubmit = async (e) => {
+        e.preventDefault(); // prevent browser reload
+        const config = {
+            url: 'http://localhost:3000/register/complete',  //url a donde regresar
+            handleCodeInApp: true,
+        };
+
+        await auth.sendSignInLinkToEmail(email, config);
+        toast.info(
+            `Correo enviado a ${email}. Da click en el enlace para finalizar tu registro.`);
+        
+            //hay que guardar el email en local storage para que no tener que meter otro.
+            window.localStorage.setItem('emailForRegistration', email)
+            //clear state
+            setEmail('');
+
+        };
+  
     const registerForm = () => <form onSubmit={handleSubmit}>
         <input type="email" 
         className= "form-control" 
@@ -20,9 +36,9 @@ import {auth} from '../../firebase'
         autoFocus
         />
 
-        <Button type="submit" className="btn btn-raised">
+        <button type="submit" className="btn btn-raised">
             Register
-        </Button>
+        </button>
        
     </form>
 
@@ -33,6 +49,8 @@ import {auth} from '../../firebase'
                 <div className="col-md-6 offset-md-3">
 
                     <h4>Register</h4>
+                    <ToastContainer  />
+                    
                     {registerForm()}
 
                 </div>
