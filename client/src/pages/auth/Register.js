753512ed37a5c8d1dc4fa18d2edb0e4f9be94d6e
JaimeSolisS <jaimesolissoto@me.com>
2020-11-11 19:51:31 -0700

    Create Register Form

--- client/src/pages/auth/Register.js
+++ client/src/pages/auth/Register.js
@@ -1,10 +1,43 @@
-import React from 'react'
+import { Button } from 'antd';
+import React, {useState} from 'react'
+import styled from 'styled-components'
 
  const Register = () => {
+
+  
+     const [email, setEmail] = useState("");
+
+    const handleSubmit = () => {
+        //
+    };
+    const registerForm = () => <form onSubmit={handleSubmit}>
+        <input type="email" 
+        className= "form-control" 
+        value={email} 
+        onChange={e => setEmail(e.target.value)}
+        autoFocus
+        />
+
+        <Button type="submit" className="btn btn-raised">
+            Register
+        </Button>
+       
+    </form>
+
      return (
-         <div>
-             <p>Register</p>
-         </div>
+       
+        <div className="container p-5">
+            <div className="row">
+                <div className="col-md-6 offset-md-3">
+
+                    <h4>Register</h4>
+                    {registerForm()}
+
+                </div>
+
+            </div>
+
+        </div>
      )
  }
 
