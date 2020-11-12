6a7cb5cf040faab7d4a9e76b7fe4f7c2ad92a74a
JaimeSolisS <jaimesolissoto@me.com>
2020-11-11 17:13:55 -0700

    Feat Nav header
    Link Home, Login and Register
    npm i @ant-design/icons
    npm i antd
    npm i styled-components
    Bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -5,16 +5,19 @@ import {Switch, Route} from "react-router-dom"
 import Home from './pages/Home'
 import Register from './pages/auth/Register'
 import Login from './pages/auth/Login'
+import  Header  from './components/nav/Header'
 
 const App = () => {
 
   return (
+    <>
+    <Header/>
     <Switch>
       <Route exact path="/" component={Home}/>
       <Route exact path="/login" component={Login}/>
       <Route exact path="/register" component={Register}/>
     </Switch>
-
+  </>
   )
 }
 
