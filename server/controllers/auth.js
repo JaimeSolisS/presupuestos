6ad8d69050e617facd7058c1b809967fad1dbb0a
JaimeSolisS <jaimesolissoto@me.com>
2020-11-14 23:41:37 -0700

    Fix nodemon used instead of node
    Server automatically restart when code changes.

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,6 +1,6 @@
 
 exports.createOrUpdateUser =  (request, response) => {
     response.json({
-        data: 'hit create-or-update-user API endpoint',
+        data: 'hit create-or-update-user API endpoint updated',
     })
 }
