e910e9095a148c926f465b897a66c62185ac7abe
JaimeSolisS <jaimesolissoto@me.com>
2020-11-14 22:16:44 -0700

    Server Setup
    npm init -y
    npm i express body-parser mongoose cors morgan express-jwt firebase-admin jsonwebtoken nodemon dotenv
    
    node_modules and .env are not commited.
    
    api response is working but to see changes the server must be restarted.

---
+++ server/server.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+require('dotenv').config()
+
+//app
+const app = express()
+
+//db
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true, 
+    useCreateIndex: true, 
+    useFindAndModify: true, 
+
+}).then(() => console.log('DB Connected'))
+.catch(error => console.log('Error:', error));
+
+//middlewares
+app.use(morgan('dev')); 
+app.use(bodyParser.json({limit: '3mb'}));
+app.use(cors()); 
+
+//route 
+app.get('/api', (request, response) => {
+    response.json({
+        data: 'Esto funciona update'
+    })
+})
+
+//port 
+const port = process.env.PORT || 8000;
+app.listen(port, () => console.log('Server is running on port', port));
