244d40ad78c759554a8be5aea52d75df1a17ad9f
JaimeSolisS <jaimesolissoto@me.com>
2020-11-15 23:42:08 -0700

    Feat Create User in Database

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,6 +1,18 @@
+const User = require('../models/user')
 
-exports.createOrUpdateUser =  (request, response) => {
-    response.json({
-        data: 'hit create-or-update-user API endpoint updated',
-    })
-}
+exports.createOrUpdateUser = async (request, response) => {
+    const {email} = request.user; 
+
+    const user = await User.findOneAndUpdate({email: email}, {new: true})
+
+    if (user) {
+        console.log('User update', user)
+        response.json(user)
+    } else {
+        console.log('User create', user)
+        const newUser = await new User({
+            email, 
+        }).save();
+        response.json(newUser); 
+    }
+};
--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,7 +1,16 @@
 const { request } = require('express');
 const admin = require('../firebase'); 
 
-exports.authCheck = (request, response, nextMethod) => {
-    console.log(request.headers); // el token
-    nextMethod();
+exports.authCheck = async (request, response, nextMethod) => {
+    //console.log(request.headers); // el token
+    try {
+        const firebaseUser = await admin.auth().verifyIdToken(request.headers.authtoken)
+       // console.log('Firebase user authcheckeck', firebaseUser); works
+        request.user = firebaseUser;
+        nextMethod();
+    } catch (error) {
+        response.status(401).json({
+            error: 'Token expirado o inválido', 
+        });
+    }
 }
