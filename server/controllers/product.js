38b16c8f958b6b59142309d0c29222849de21634
DanilN1 <36081046+DanilN1@users.noreply.github.com>
2020-11-30 12:37:11 -0700

    Controller budget y product
    controllers budget & products

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -8,23 +8,34 @@ exports.createProduct = async (request, response) => {
         return response.status(200).json(product);
 };
 
+exports.updateProduct = async (request, response) => {
+    const {id, hidden} = request.body;
+    const product = await Product.findOneAndUpdate({_id: id}, { $set: {hidden: hidden} },{new: true});
+    if (product) {
+        console.log('Budget update', product);
+        response.status(202).json(product);
+    } else {
+        return response.sendStatus(404); 
+    }
+};
+
 exports.findOneProduct = async (request, response) => {
     await Product.findOne({_id: request.params.id}).exec((error, product) => {
         if (error) throw new Error(error);
         response.json(product); 
     })
-}
+};
 
 exports.getAllProducts = async (request, response) => {
     await Product.find().exec((error,product) => {
         if (error) throw new Error(error);
         response.json(product); 
     })
-}
+};
 
 exports.findProductsByCategory = async (request, response) => {
     await Product.find({category: request.params.category}).exec((error, product) => {
         if (error) throw new Error(error);
         response.json(product); 
     })
-}
+};
--- server/routes/budget.js
+++ server/routes/budget.js
@@ -2,8 +2,12 @@ const express = require('express');
 
 const router = express.Router();
 const {authCheck, adminCheck} = require('../middlewares/auth')
-const {createBudget, updateBudget} = require('../controllers/budget');
+const {createBudget, updateBudget, findBudgetById, findBudgetsByUserId, deleteBudget, findAllBudgets} = require('../controllers/budget');
 
 router.post('/create-budget', createBudget);
 router.post('/update-budget', updateBudget);
+router.delete('/delete-budget', deleteBudget);
+router.get('/find-budget-by-id/:id', findBudgetById);
+router.get('/find-budgets',findAllBudgets);
+router.get('/find-budgets-by-userid/:userId', findBudgetsByUserId);
 module.exports= router;
