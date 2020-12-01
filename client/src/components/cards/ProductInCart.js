0f5d1242e6d2ba5f582f8afd8d2651d06907eff1
JaimeSolisS <jaimesolissoto@me.com>
2020-12-01 15:09:49 -0700

    Remove item from cart

--- client/src/components/cards/ProductInCart.js
+++ client/src/components/cards/ProductInCart.js
@@ -1,6 +1,32 @@
 import React from "react";
+import {CloseOutlined} from '@ant-design/icons'
+import { useDispatch } from "react-redux";
 
 const ProductInCart = ({ p }) => {
+
+    let dispatch = useDispatch();
+
+    const handleRemove = () => {
+         // console.log(p._id, "to remove");
+    let cart = [];
+
+    if (typeof window !== "undefined") {
+      if (localStorage.getItem("cart")) {
+        cart = JSON.parse(localStorage.getItem("cart"));
+      }
+      cart.map((product, i) => {
+        if (product._id === p._id) {
+          cart.splice(i, 1);
+        }
+      });
+
+      localStorage.setItem("cart", JSON.stringify(cart));
+      dispatch({
+        type: "ADD_TO_CART",
+        payload: cart,
+      });
+    }
+    }
   return (
     <tbody>
       <tr>
@@ -9,7 +35,10 @@ const ProductInCart = ({ p }) => {
         <td>{p.description}</td>
         <td>{p.cantidad}</td>
         <td>${p.total / p.cantidad}</td>
-        <td>Delete Icon</td>
+        <td className='text-center'>
+        <CloseOutlined onClick={handleRemove} className='text-danger poiner'>
+            
+            </CloseOutlined></td>
       </tr>
     </tbody>
   );
