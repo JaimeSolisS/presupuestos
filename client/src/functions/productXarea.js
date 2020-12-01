bd1076df75c430629b6767523d7273308d2c5765
Ivan Bustillos <ivan@cynch.me>
2020-11-30 21:46:35 -0600

    Precio en producto

--- client/src/functions/productXarea.js
+++ client/src/functions/productXarea.js
@@ -2,6 +2,6 @@ import axios from "axios";
 export const createPricePerArea = (price) =>{
     return axios.post(`${process.env.REACT_APP_API}/create-price-per-area`, price);
 }
-export const getPriceQuantityByProductId = (prodId) =>{
+export const getPriceAreaByProductId = (prodId) =>{
     return axios.get(`${process.env.REACT_APP_API}/get-price-area-by-productid/${prodId}`)
 }
